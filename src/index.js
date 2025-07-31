87ed1a97a839f14c40668902dcc27b6005feffa3
JesterAV <winchesrers@gmail.com>
2025-07-31 13:20:30 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const ones = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  if (number < 20) {
+    return ones[number];
+  }
+
+  if (number < 100) {
+    const ten = tens[Math.floor(number / 10)];
+    const one = ones[number % 10];
+    return one === 'zero' ? ten : `${ten} ${one}`;
+  }
+
+  const hundred = ones[Math.floor(number / 100)];
+  const remainder = number % 100;
+
+  if (remainder === 0) {
+    return `${hundred} hundred`;
+  }
+  return `${hundred} hundred ${toReadable(remainder)}`;
 };
